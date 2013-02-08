11faa21c43f03c743a02f41cacfd22eb25429424
Philippe Bernery <philippe.bernery@gmail.com>
2013-02-08 13:55:55 +0100

    Initial Commit

---
+++ PBYouTubeVideoViewController/PBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PBAppDelegate.h
+//  PBYouTubeVideoViewController
+//
+//  Created by Philippe Bernery on 08/02/13.
+//  Copyright (c) 2013 Philippe Bernery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PBYouTubeVideoViewController/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  PBYouTubeVideoViewController
+//
+//  Created by Philippe Bernery on 08/02/13.
+//  Copyright (c) 2013 Philippe Bernery. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "PBAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([PBAppDelegate class]));
+    }
+}
