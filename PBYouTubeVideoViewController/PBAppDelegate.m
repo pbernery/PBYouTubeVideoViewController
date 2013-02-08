6de5967736af81c655b8dbfa72a4bf6b2252b3a8
Philippe Bernery <philippe.bernery@gmail.com>
2013-02-08 19:23:17 +0100

    AppDelegate is set as a delete to YouTube view controller.

--- PBYouTubeVideoViewController/PBAppDelegate.m
+++ PBYouTubeVideoViewController/PBAppDelegate.m
@@ -9,6 +9,10 @@
 #import "PBAppDelegate.h"
 #import "PBYouTubeVideoViewController.h"
 
+
+@interface PBAppDelegate () <PBYouTubeVideoViewControllerDelegate>
+@end
+
 @implementation PBAppDelegate
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
@@ -17,10 +21,16 @@
     self.window.backgroundColor = [UIColor blackColor];
 
     PBYouTubeVideoViewController *viewController = [[PBYouTubeVideoViewController alloc] initWithVideoId:@"9bZkp7q19f0"];
+    viewController.delegate = self;
     self.window.rootViewController = viewController;
 
     [self.window makeKeyAndVisible];
     return YES;
+}
+
+- (void)youTubeVideoViewController:(PBYouTubeVideoViewController *)viewController didReceiveEventNamed:(NSString *)eventName eventData:(NSString *)eventData
+{
+    NSLog(@"event: %@ - value: %@", eventName, eventData ? eventData : @"");
 }
 
 @end
